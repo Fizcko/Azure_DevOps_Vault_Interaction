1f0103c35277ab153caf3c54ec9c2d23073ee575
Charlie Hamel <chamel@shipbob.com>
2022-08-11 17:04:05 -0500

    adding new fields to task json

--- test/kv_read.js
+++ test/kv_read.js
@@ -60,6 +60,8 @@ tr.setInput("strPrefixType", conf.kv_read.strPrefixType);
 tr.setInput("strSecretPath", conf.kv_read.strSecretPath ? conf.kv_read.strSecretPath : "");
 tr.setInput("replaceCR", conf.kv_read.replaceCR ? conf.kv_read.replaceCR : false);
 tr.setInput("strCRPrefix", conf.kv_read.strCRPrefix ? conf.kv_read.strCRPrefix : "");
+tr.setInput("objectSplit", conf.kv_read.objectSplit ? conf.kv_read.objectSplit : false);
+tr.setInput("objectSeperator", conf.kv_read.objectSeperator ? conf.kv_read.objectSeperator : "");
 
 tr.registerMock('azure-pipelines-task-lib/toolrunner', require('azure-pipelines-task-lib/mock-toolrunner'));
 tr.run();
